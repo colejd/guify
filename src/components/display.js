94579957244f7385ebc4f71aa956cadf6f0161c8
Jonathan Cole <joncole04412@gmail.com>
2021-11-15 13:42:25 -0500

    Improve display disabled styling

--- src/components/display.js
+++ src/components/display.js
@@ -1,6 +1,6 @@
 import ComponentBase from "./component-base.js";
 
-import css from "dom-css";
+import "./display.css";
 
 import { default as LabelPartial } from "./partials/label";
 
@@ -14,26 +14,10 @@ export default class Display extends ComponentBase {
         this.label = LabelPartial(this.container, opts.label, theme);
 
         this.text = this.container.appendChild(document.createElement("div"));
-        css(this.text, {
-            display: "inline-block",
-            height: "unset",
-            width: `calc(100% - ${theme.sizing.labelWidth})`,
-            border: "none",
-            //background: theme.background2,
-            color: theme.colors.textSecondary,
-            fontFamily: "inherit",
-            "box-sizing": "border-box",
-            "-moz-box-sizing": "border-box",
-            "-webkit-box-sizing": "border-box",
-            verticalAlign: "sub",
-            "line-height": theme.sizing.componentHeight,
-            "user-select": "text"
-        });
+        this.text.classList.add("guify-display");
 
         // Add ARIA attribute to text based on label text
         if(opts.label) this.text.setAttribute("aria-label", opts.label);
-
-
     }
 
     SetValue(value) {
