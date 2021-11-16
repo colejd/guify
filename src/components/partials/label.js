156c9eea80aa0f5b1a4786e63ad01d1ee7a1611a
Jonathan Cole <joncole04412@gmail.com>
2021-11-16 14:38:20 -0500

    Simplify label, add presumptive fix for bad component heights on some pages

--- src/components/partials/label.js
+++ src/components/partials/label.js
@@ -2,11 +2,8 @@ import "./label.css";
 
 // eslint-disable-next-line no-unused-vars
 export default (root, text, theme) => {
-    var background = root.appendChild(document.createElement("div"));
-    background.classList.add("guify-component-label");
-
-    var label = background.appendChild(document.createElement("div"));
-    label.classList.add("guify-component-label-text");
+    var label = root.appendChild(document.createElement("div"));
+    label.classList.add("guify-component-label");
     label.innerHTML = text;
     return label;
 };
